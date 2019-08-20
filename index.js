e626eed9133eae39fa558966ef31e2ed0b683d46
Robert DeLuca <robertdeluca19@gmail.com>
2019-08-20 17:33:54 -0500

    fix: Disable Percy after first failed snapshot POST (#1)
    * fix: Disable Percy after the first failed POST request
    
    We don't want to fill their test suite with error logs. If the first request
    fails we can safely disable Percy.
    
    * Update test workflow to have non-percy enable tests
    
    * Update error message

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const {
 const sdkPkg = require("./package.json");
 const testCafePkg = require("testcafe/package.json");
 
+let isPercyRunning = true;
+
 async function percySnapshot(test, snapshotName, snapshotOptions) {
+  if (!isPercyRunning) {
+    return;
+  }
+
   let getURL = ClientFunction(() => window.location.href).with({
     boundTestRun: test
   });
@@ -62,7 +68,8 @@ async function postDomSnapshot(name, domSnapshot, url, options) {
   });
 
   if (!postSuccess) {
-    console.log(`[percy] Error posting snapshot to agent`);
+    console.log(`[percy] Error posting snapshot to agent, disabling Percy`);
+    isPercyRunning = false;
   }
 }
 
