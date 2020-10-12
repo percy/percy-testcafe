c4ac2f9488c0973f9060d8ab2addd921457d7c3a
Wil Wilsman <wil@wilwilsman.com>
2020-10-12 18:40:11 -0500

    ✨ Use @percy/cli

--- index.js
+++ index.js
@@ -1,84 +1,76 @@
-const { readFileSync } = require("fs");
-const { ClientFunction } = require("testcafe");
-const {
-  postSnapshot,
-  agentJsFilename
-} = require("@percy/agent/dist/utils/sdk-utils");
+import fs from 'fs';
+import fetch from 'node-fetch';
+import log from '@percy/logger';
+import { t } from 'testcafe';
 
-const sdkPkg = require("./package.json");
-const testCafePkg = require("testcafe/package.json");
+// Collect client and environment information
+import sdkPkg from './package.json';
+import testCafePkg from 'testcafe/package.json';
+const CLIENT_INFO = `${sdkPkg.name}/${sdkPkg.version}`;
+const ENV_INFO = `${testCafePkg.name}/${testCafePkg.version}`;
 
-let isPercyRunning = true;
+// Maybe get the CLI API address from the environment
+const { PERCY_CLI_API = 'http://localhost:5338/percy' } = process.env;
 
-async function percySnapshot(test, snapshotName, snapshotOptions) {
-  if (!isPercyRunning) {
-    return;
-  }
-
-  let getURL = ClientFunction(() => window.location.href).with({
-    boundTestRun: test
-  });
-  // Inject the JS that captures and serializes the DOM
-  let agentFileContents = readFileSync(agentJsFilename()).toString();
-  // TODO, capture the case where the script doesn't inject and add a nice error
-  let loadScriptFile = ClientFunction(
-    () => {
-      var script = document.createElement("script");
-
-      script.text = agentFileContents;
-      document.body.append(script);
-
-      return true;
-    },
-    { dependencies: { agentFileContents: agentFileContents } }
-  ).with({ boundTestRun: test });
-
-  let getDOMSnapshot = ClientFunction(
-    () => {
-      let percyAgentClient = new PercyAgent({
-        handleAgentCommunication: false
-      });
-
-      return percyAgentClient.snapshot(snapshotName, snapshotOptions);
-    },
-    {
-      dependencies: {
-        snapshotName: snapshotName,
-        snapshotOptions: snapshotOptions
-      }
+// Check if Percy is enabled using the healthcheck endpoint
+async function isPercyEnabled() {
+  if (isPercyEnabled.result == null) {
+    try {
+      let response = await fetch(`${PERCY_CLI_API}/healthcheck`);
+      isPercyEnabled.result = response.ok;
+    } catch (err) {
+      isPercyEnabled.result = false;
+      log.debug(err);
     }
-  ).with({ boundTestRun: test });
 
-  await loadScriptFile();
-  let url = await getURL();
-  // Get the serialized DOM from the browser
-  let domSnapshot = await getDOMSnapshot();
-  // Send it off to `@percy/agent` for asset discovery
-  await postDomSnapshot(snapshotName, domSnapshot, url, snapshotOptions);
-}
-
-async function postDomSnapshot(name, domSnapshot, url, options) {
-  let postSuccess = await postSnapshot({
-    url,
-    name,
-    domSnapshot,
-    clientInfo: clientInfo(),
-    environmentInfo: environmentInfo(),
-    ...options
-  });
-
-  if (!postSuccess) {
-    console.log(`[percy] Error posting snapshot to agent, disabling Percy`);
-    isPercyRunning = false;
+    if (isPercyEnabled.result === false) {
+      log.info('Percy is not running, disabling snapshots');
+    }
   }
-}
 
-function clientInfo() {
-  return `@percy/testcafe-${sdkPkg.version}`;
-}
+  return isPercyEnabled.result;
+};
 
-function environmentInfo() {
-  return `testcafe-${testCafePkg.version}`;
+async function percySnapshot(name, options) {
+  if (!name) throw new Error('The `name` argument is required.');
+  if (!(await isPercyEnabled())) return;
+
+  try {
+    // Inject the DOM serialization script
+    /* eslint-disable-next-line no-new-func */
+    await t.eval(new Function(fs.readFileSync(require.resolve('@percy/dom'), 'utf-8')));
+
+    // Serialize and capture the DOM
+    /* istanbul ignore next: no instrumenting injected code */
+    let domSnapshot = await t.eval(() => {
+      /* eslint-disable-next-line no-undef */
+      return PercyDOM.serialize(options);
+    }, { dependencies: { options } });
+
+    /* istanbul ignore next: no instrumenting injected code */
+    let documentURL = await t.eval(() => document.URL);
+
+    // Post the DOM to the snapshot endpoint with snapshot options and other info
+    let response = await fetch(`${PERCY_CLI_API}/snapshot`, {
+      method: 'POST',
+      body: JSON.stringify({
+        ...options,
+        environmentInfo: ENV_INFO,
+        clientInfo: CLIENT_INFO,
+        url: documentURL,
+        domSnapshot,
+        name
+      })
+    });
+
+    // Handle errors
+    let { success, error } = await response.json();
+    if (!success) throw new Error(error);
+  } catch (err) {
+    log.error(`Could not take DOM snapshot "${name}"`);
+    log.error(err);
+  }
 }
 
 module.exports = percySnapshot;
+module.exports.isPercyEnabled = isPercyEnabled;
