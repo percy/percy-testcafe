6fb57fa22533c37c529c7e6d2864369dd4a27e58
Callum Silcock <github@csi.lk>
2021-06-08 15:54:24 +1000

    🐛 add TestController as first param to percySnapshot types
    as per the readme
    
    resolves #108

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,8 @@
-import { SnapshotOptions } from '@percy/core';
+import { SnapshotOptions } from "@percy/core"
+import { t as TestCafe } from "testcafe"
 
 export default function percySnapshot(
+  t: typeof TestCafe,
   name: string,
   options?: SnapshotOptions
-): Promise<void>;
+): Promise<void>
