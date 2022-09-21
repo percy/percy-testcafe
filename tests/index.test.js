408ca13ec9bba25ecb23882df3cc17df3de2eeda
Robert DeLuca <robertdeluca19@gmail.com>
2022-09-21 11:29:15 -0500

    ⬆️ Update `@percy/sdk-utils` to use new mocked logger

--- tests/index.test.js
+++ tests/index.test.js
@@ -22,8 +22,8 @@ test('disables snapshots when the healthcheck fails', async t => {
   await percySnapshot(t, 'Snapshot 1');
   await percySnapshot(t, 'Snapshot 2');
 
-  expect(await helpers.get('logs')).toEqual(expect.arrayContaining([
-    'Percy is not running, disabling snapshots'
+  expect(helpers.logger.stdout).toEqual(expect.arrayContaining([
+    '[percy] Percy is not running, disabling snapshots'
   ]));
 });
 
@@ -44,7 +44,7 @@ test('handles snapshot errors', async t => {
   await helpers.test('error', '/percy/snapshot');
   await percySnapshot(t, 'Snapshot 1');
 
-  expect(await helpers.get('logs')).toEqual(expect.arrayContaining([
-    'Could not take DOM snapshot "Snapshot 1"'
+  expect(helpers.logger.stderr).toEqual(expect.arrayContaining([
+    '[percy] Could not take DOM snapshot "Snapshot 1"'
   ]));
 });
